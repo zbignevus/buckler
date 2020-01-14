a406a394c86735fd0ca041969b2b1293be39b6ac
unknown <zbignevus@gmail.com>
2020-01-14 04:39:02 +0200

    updated menu-items to navigate withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,14 +1,35 @@
 import React from 'react';
-
+import { Switch, Route, Link } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 
 
+
+const TopicDetail = () => (
+  <div>
+    <h1>TOPIC DETAIL PAGE</h1>
+  </div>
+)
+
+const TopicsList = (props) => {
+  console.log(props)
+  return <div>
+    <h1>TOPICS LIST</h1>
+  </div>
+}
+
+const HatsPage = () => (
+  <h1>HATS PAGE</h1>
+)
+
 function App() {
   return (
-    <div>
-      <HomePage />
-    </div>
+    <Switch>
+      <Route exact path='/' component={HomePage} />
+      <Route exact path='/topics' component={TopicsList} />
+      <Route exact path='/hats' component={HatsPage} />
+      <Route path='/topics/:topicId' component={TopicDetail} />
+    </Switch>
   );
 }
 
--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import MenuItem from '../menu-item/menu-item.component';
 
 import './directory.styles.scss';
+import { HashRouter } from 'react-router-dom';
 
 class Directory extends Component {
     constructor() {
@@ -14,18 +15,21 @@ class Directory extends Component {
                     title: 'hats',
                     imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
                     id: 1,
+                    linkUrl: 'hats',
 
                 },
                 {
                     title: 'jackets',
                     imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
                     id: 2,
+                    linkUrl: '',
 
                 },
                 {
                     title: 'sneakers',
                     imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
                     id: 3,
+                    linkUrl: '',
 
                 },
                 {
@@ -33,6 +37,7 @@ class Directory extends Component {
                     imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
                     id: 4,
                     size: 'large',
+                    linkUrl: '',
 
                 },
                 {
@@ -40,6 +45,7 @@ class Directory extends Component {
                     imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
                     id: 5,
                     size: 'large',
+                    linkUrl: '',
                 }
             ]
 
@@ -50,9 +56,9 @@ class Directory extends Component {
         return (
             <div className='directory-menu'>
                 {this.state.sections.map(section => {
-                    const { title, id, imageUrl, size } = section;
+                    const { id, ...otherSectionProps } = section;
 
-                    return <MenuItem key={id} size={size} title={title} imageUrl={imageUrl} />
+                    return <MenuItem key={id} {...otherSectionProps} />
 
                 }
                 )
