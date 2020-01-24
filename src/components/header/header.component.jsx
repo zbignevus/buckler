3a6b6435f86713f0a62cc11ada124df756650eee
unknown <zbignevus@gmail.com>
2020-01-24 12:59:44 +0200

    added redux state management for header component to track sign in status.

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { connect } from "react-redux";
 import "./header.styles.scss";
 import { ReactComponent as Logo } from "../../assets/crown.svg";
 
 import { auth } from "../../firebase/firebase.utils";
+import { userInfo } from "os";
 
 const Header = ({ currentUser }) => (
   <div className="header">
@@ -30,4 +32,8 @@ const Header = ({ currentUser }) => (
   </div>
 );
 
-export default Header;
+const mapStateToProps = state => ({
+  currentUser: state.user.currentUser
+});
+
+export default connect(mapStateToProps)(Header);
