689aee0d97b90ef510cae7a9e93e7d6518bcc8e0
unknown <zbignevus@gmail.com>
2020-01-26 13:36:38 +0200

    Added cartItems to redux with quantity add method.

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -2,14 +2,34 @@ import React from "react";
 
 import "./collection-item.styles.scss";
 
-const CollectionItem = ({ id, name, price, imageUrl }) => (
-  <div className="collection-item">
-    <div className="image" style={{ backgroundImage: `url(${imageUrl})` }} />
-    <div className="collection-footer">
-      <span className="name">{name}</span>
-      <span className="price">{price}</span>
-    </div>
-  </div>
-);
+import { connect } from "react-redux";
+import { addItem } from "../../redux/cart/cart.actions";
 
-export default CollectionItem;
+import CustomButton from "../custom-button/custom-button.component";
+
+const mapDispatchToProps = dispatch => ({
+  addItem: item => dispatch(addItem(item))
+});
+
+const CollectionItem = ({ item, addItem }) => {
+  const { name, price, imageUrl } = item;
+
+  return (
+    <div className="collection-item">
+      <div className="image" style={{ backgroundImage: `url(${imageUrl})` }} />
+
+      <div className="collection-footer">
+        <span className="name">{name}</span>
+        <span className="price">{price}</span>
+      </div>
+      <CustomButton onClick={() => addItem(item)} inverted>
+        Add to cart
+      </CustomButton>
+    </div>
+  );
+};
+
+export default connect(
+  null,
+  mapDispatchToProps
+)(CollectionItem);
