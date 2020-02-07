9ce1e7a4fe932678ea66bb341d3650736f80f35b
unknown <zbignevus@gmail.com>
2020-02-07 12:59:37 +0200

    Added Stripe Payment functionality to Checkout page

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -5,6 +5,8 @@ import { createStructuredSelector } from "reselect";
 
 import CheckoutItem from "../../components/checkout-item/checkout-item.component";
 
+import StripeCheckoutButton from "../../components/stripe-button/stripe-button.component";
+
 import {
   selectCartItems,
   selectCartTotal
@@ -35,8 +37,14 @@ const CheckoutPage = ({ cartItems, total }) => (
       <CheckoutItem key={cartItem.id} cartItem={cartItem} />
     ))}
     <div className="total">
-      <span>TOTAL: ${total} </span>
+      <span>TOTAL: €{total} </span>
     </div>
+    <div className="test-warning">
+      *Please use the following test credit card for payments:*
+      <br />
+      4242 4242 4242 4242 - Exp: 02/20 - CW:123
+    </div>
+    <StripeCheckoutButton price={total} />
   </div>
 );
 
